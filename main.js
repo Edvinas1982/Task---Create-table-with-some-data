b960aeb713ce5690d150ca0555e07b5086e87e9f
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2023-01-12 13:23:01 +0200

    geting_data_calculating_speed

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var button = document.getElementById('button');
+button.addEventListener('click', function() {
+    var date = document.getElementById('date').value;
+    var plate = document.getElementById('plate').value;
+    var distance = document.getElementById('distance').value;
+    var time = document.getElementById('time').value;
+    
+    distance = parseInt(distance);
+    time = parseInt(time);
 
+    speed = distance / time * 3,6; 
+    var tbody = document.querySelector('tbody');
+    var data = [date, plate, distance, time, speed];
+    console.log(data);
+})
